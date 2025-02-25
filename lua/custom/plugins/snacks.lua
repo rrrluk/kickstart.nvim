4301e70fde050ff82fead6ca7caaa496d143eca9
Pertti Johanson <pertti.johanson@nexigroup.com>
2025-02-25 19:57:34 +0200

    some snacks working

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,6 +1,9 @@
 return {
   'folke/snacks.nvim',
-  ---@type snacks.Config
+  priority = 1000,
+  lazy = false,
+
+  -- NOTE: Options
   opts = {
     explorer = {
       -- your explorer configuration comes here
@@ -15,5 +18,76 @@ return {
         },
       },
     },
+  },
+  -- NOTE: Keymaps
+  keys = {
+    {
+      '<leader>gg',
+      function()
+        require('snacks').lazygit()
+      end,
+      desc = 'Lazygit',
+    },
+    {
+      '<leader>gl',
+      function()
+        require('snacks').lazygit.log()
+      end,
+      desc = 'Lazygit Logs',
+    },
+    {
+      '<leader>e',
+      function()
+        require('snacks').explorer()
+      end,
+      desc = 'Open Snacks Explorer',
+    },
+    {
+      '<leader>bd',
+      function()
+        require('snacks').bufdelete()
+      end,
+      desc = 'Delete or Close Buffer  (Confirm)',
+    },
+
+    -- Snacks Picker
+    {
+      '<leader><leader>',
+      function()
+        require('snacks').picker.files()
+      end,
+      desc = 'Find Files (Snacks Picker)',
+    },
+    {
+      '<leader>fc',
+      function()
+        require('snacks').picker.files { cwd = vim.fn.stdpath 'config' }
+      end,
+      desc = 'Find Config File',
+    },
+    {
+      '<leader>/',
+      function()
+        require('snacks').picker.grep()
+      end,
+      desc = 'Grep word',
+    },
+    {
+      '<leader>fws',
+      function()
+        require('snacks').picker.grep_word()
+      end,
+      desc = 'Search Visual selection or Word',
+      mode = { 'n', 'x' },
+    },
+    {
+      '<leader>fk',
+      function()
+        require('snacks').picker.keymaps { layout = 'ivy' }
+      end,
+      desc = 'Search Keymaps (Snacks Picker)',
+    },
+
+    -- Other Utils
   },
 }
