7b03e3bd90919d066726928ff8c3f44d52705713
Pertti Johanson <pertti.johanson@nexigroup.com>
2025-02-27 16:39:52 +0200

    added autopairs and bufferline

--- lua/custom/keymaps/keymaps.lua
+++ lua/custom/keymaps/keymaps.lua
@@ -1,5 +1,4 @@
 return {
-  vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = 'Toggle Neotree' }),
   vim.keymap.set('n', '<S-h>', ':bprev<CR>', { desc = 'Select previous buffer' }),
   vim.keymap.set('n', '<S-l>', ':bnext<CR>', { desc = 'Select next buffer' }),
   vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = 'Delete buffer' }),
